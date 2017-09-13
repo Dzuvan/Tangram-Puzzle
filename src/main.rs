f95c29d36113467f05976f526ea6b2dd85fe50ef
dzuvan <hrnjak.jovan1@gmail.com>
2017-09-13 18:52:25 +0200

    Fixed typos.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 extern crate sdl2;
 extern crate rand;
 
-use std::time::Duration;
 use std::path::Path;
 use std::borrow::Cow;
 
@@ -11,7 +10,7 @@ pub mod board;
 
 use sdl2::pixels::Color;
 use sdl2::video::Window;
-use sdl2::audio::{ AudioStatus, AudioCallback, AudioSpecDesired,AudioSpecWAV,AudioCVT };
+use sdl2::audio::{ AudioCallback, AudioSpecDesired,AudioSpecWAV,AudioCVT };
 use sdl2::render::Canvas;
 use sdl2::rect::Rect;
 use sdl2::AudioSubsystem;
@@ -191,6 +190,7 @@ fn main() {
             l.handle_events(&event);
             r.handle_events(&event);
 
+    for solution in &solutions {
             if s.check_win(&solution[0])&&
             u.check_win(&solution[1])&&
             i.check_win(&solution[2])&&
